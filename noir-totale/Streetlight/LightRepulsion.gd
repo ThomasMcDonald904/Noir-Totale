e35ccf484c792fc7a42c3060a2c167a62ec0f405
Thomas McDonald <53022931+ThomasMcDonald904@users.noreply.github.com>
2026-02-09 16:24:47 -0500

    Added ui stuff to explaining scene

--- noir-totale/Streetlight/LightRepulsion.gd
+++ noir-totale/Streetlight/LightRepulsion.gd
@@ -3,7 +3,7 @@ extends Area3D
 @export var repulsion_strength := 2
 @export var inner_boost := 250.0   # strong snap near the center
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	for body in get_overlapping_bodies():
 		if body.has_method("apply_repulsion_reversal"):
 			var to_body = body.global_position - global_position
